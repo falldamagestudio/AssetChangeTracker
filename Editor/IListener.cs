9214db3d638efb7ec81d39904afac004366d5d13
Mikael Kalms <mikael@kalms.org>
2019-03-22 16:06:40 +0100

    Spaces to tabs

--- Editor/IListener.cs
+++ Editor/IListener.cs
@@ -2,37 +2,37 @@
 
 namespace AssetChangeTracker
 {
-    /// <summary>
-    /// Asset database activity listener interface.
-    /// This provides a minimal API that allows monitoring of changes to a set of assets of a given type.
-    /// </summary>
-    public interface IListener
-    {
-        /// <summary>
-        /// Called when a listener is registered.
-        /// Provides the entire set of assets of the matching type that is present in the AssetDatabase at the time of registration.
-        /// </summary>
-        void OnExists(HashSet<string> assetPaths);
+	/// <summary>
+	/// Asset database activity listener interface.
+	/// This provides a minimal API that allows monitoring of changes to a set of assets of a given type.
+	/// </summary>
+	public interface IListener
+	{
+		/// <summary>
+		/// Called when a listener is registered.
+		/// Provides the entire set of assets of the matching type that is present in the AssetDatabase at the time of registration.
+		/// </summary>
+		void OnExists(HashSet<string> assetPaths);
 
-        /// <summary>
-        /// Called whenever a new asset is added to the AssetDatabase.
-        /// </summary>
-        void OnAdded(string assetPath);
+		/// <summary>
+		/// Called whenever a new asset is added to the AssetDatabase.
+		/// </summary>
+		void OnAdded(string assetPath);
 
-        /// <summary>
-        /// Called whenever the contents of an asset (which already is known) may have changed.
-        /// </summary>
-        void OnChanged(string assetPath);
+		/// <summary>
+		/// Called whenever the contents of an asset (which already is known) may have changed.
+		/// </summary>
+		void OnChanged(string assetPath);
 
-        /// <summary>
-        /// Called whenever an asset is moved from one location to another.
-        /// </summary>
-        void OnMoved(string assetSourcePath, string assetTargetPath);
+		/// <summary>
+		/// Called whenever an asset is moved from one location to another.
+		/// </summary>
+		void OnMoved(string assetSourcePath, string assetTargetPath);
 
-        /// <summary>
-        /// Called whenever an asset has been removed from the AssetDatabase.
-        /// This is either because the Unity editor has been instructed to delete the file, or because an external program has removed it.
-        /// </summary>
-        void OnDeleted(string assetPath);
-    }
+		/// <summary>
+		/// Called whenever an asset has been removed from the AssetDatabase.
+		/// This is either because the Unity editor has been instructed to delete the file, or because an external program has removed it.
+		/// </summary>
+		void OnDeleted(string assetPath);
+	}
 }
--- Editor/TrackedAssetType.cs
+++ Editor/TrackedAssetType.cs
@@ -4,24 +4,24 @@ using UnityEngine.Assertions;
 
 namespace AssetChangeTracker
 {
-    /// <summary>
-    /// Handles asset database notifications for an individual asset type.
-    /// </summary>
-    public class TrackedAssetType
+	/// <summary>
+	/// Handles asset database notifications for an individual asset type.
+	/// </summary>
+	public class TrackedAssetType
 	{
 		private HashSet<string> AssetPaths = new HashSet<string>();
 		private HashSet<IListener> Listeners = new HashSet<IListener>();
 
-        /// <param name="assetType">Type of asset to track.</param>
-        /// <param name="assetPaths">Set of paths to assets that exist at the time the tracker is created.</param>
+		/// <param name="assetType">Type of asset to track.</param>
+		/// <param name="assetPaths">Set of paths to assets that exist at the time the tracker is created.</param>
 		public TrackedAssetType(Type assetType, HashSet<string> assetPaths)
 		{
 			AssetPaths = assetPaths;
 		}
 
-        /// <summary>
-        /// Adds listener for this asset type.
-        /// </summary>
+		/// <summary>
+		/// Adds listener for this asset type.
+		/// </summary>
 		public void AddListener(IListener listener)
 		{
 			Assert.IsFalse(Listeners.Contains(listener), "Attempted to add the same listener twice for the same type. This is not allowed.");
@@ -30,21 +30,21 @@ namespace AssetChangeTracker
 			listener.OnExists(AssetPaths);
 		}
 
-        /// <summary>
-        /// Removes listener for this asset type.
-        /// Returns true if there still are other listeners present, or false if there are none.
-        /// </summary>
+		/// <summary>
+		/// Removes listener for this asset type.
+		/// Returns true if there still are other listeners present, or false if there are none.
+		/// </summary>
 		public bool RemoveListener(IListener listener)
 		{
 			Assert.IsTrue(Listeners.Contains(listener), "Attempted to remove a listener for type which is not currently registered. This is not allowed.");
 			Listeners.Remove(listener);
-            return Listeners.Count > 0;
-        }
+			return Listeners.Count > 0;
+		}
 
 		public void OnReimported(string assetPath)
 		{
-            // Translate Reimported to either Added or Changed,
-            //   depending on whether the asset already is known to the tracker
+			// Translate Reimported to either Added or Changed,
+			//   depending on whether the asset already is known to the tracker
 			if (!AssetPaths.Contains(assetPath))
 			{
 				AssetPaths.Add(assetPath);
@@ -74,7 +74,7 @@ namespace AssetChangeTracker
 			// Therefore it must ignore delete operations on assets which it isn't currently tracking
 			if (AssetPaths.Contains(assetPath))
 			{
-                AssetPaths.Remove(assetPath);
+				AssetPaths.Remove(assetPath);
 
 				foreach (IListener listener in Listeners)
 					listener.OnDeleted(assetPath);
